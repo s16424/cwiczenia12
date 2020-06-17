10326d7bec8fb4e679a62c1f8571ed76b5f0e660
unknown <s16424@pjwstk.edu.pl>
2020-06-17 18:04:55 +0200

    cwiczenia12

---
+++ zadania_przyg/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace zadania_przyg.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: /HelloWorld/Welcome
+        //public string Welcome()
+        //{
+        //    return "This is the welcome action method";
+        //}
+        public IActionResult Welcome(string name, int numTimes=1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+    }
+}
---
+++ zadania_przyg/Models/Movie.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zadania_przyg.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength =3)]
+        [Required]
+        public string Title { get; set; }   
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        
+    }
+}
